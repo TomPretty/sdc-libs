491bffa68810daf992e41dc07d104546cf39aabb
Tom Pretty <tompretty2@gmail.com>
2022-04-20 10:55:56 +0100

    add server

---
+++ apps/server/src/main.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+
+const app = express();
+
+app.post('/header', express.json(), (req, res) => {
+  const body = req.body;
+
+  console.log({ body });
+
+  res.status(203).json({ header: 'Thank you' });
+});
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+server.on('error', console.error);
