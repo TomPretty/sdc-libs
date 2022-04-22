c91857b1710211e3706168c8e31bf203a1fe9238
TomPretty <tompretty2@gmail.com>
2022-04-22 12:15:51 +0100

    factor out banner controller

--- apps/server/src/banner-assignment/router.spec.ts
+++ apps/server/src/banner-assignment/router.spec.ts
@@ -1,68 +1,27 @@
-import { bannerRequestFactory, bannerTestFactory } from '@sdc-libs/factories';
-import { BannerTest } from '@sdc-libs/types';
+import { bannerRequestFactory } from '@sdc-libs/factories';
 import { getRouter } from './router';
 import * as express from 'express';
 import supertest = require('supertest');
 
-describe('/banner', () => {
-  it('returns a banner if there is one that matches the targeting', async () => {
-    const st = getSuperTest(
-      getRouter({
-        getBannerTests: getBannerTests(
-          bannerTestFactory.build({
-            name: 'EXAMPLE_TEST',
-            targeting: { edition: 'UK' },
-          })
-        ),
-      })
-    );
-    const bannerRequest = bannerRequestFactory.build({ edition: 'UK' });
+describe('router', () => {
+  const router = getRouter({ getBannerTests: () => Promise.resolve([]) });
+  const app = express();
+  app.use('/', router);
+  const request = supertest(app);
 
-    const response = await st.post('/banner').send(bannerRequest);
+  it('returns a 200 for a valid request', async () => {
+    const bannerRequest = bannerRequestFactory.build();
+
+    const response = await request.post('/').send(bannerRequest);
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.assigned).toBeTruthy();
-  });
-
-  it("doesn't return a banner if there isn't one that matches the targeting", async () => {
-    const st = getSuperTest(
-      getRouter({
-        getBannerTests: getBannerTests(
-          bannerTestFactory.build({
-            name: 'EXAMPLE_TEST',
-            targeting: { edition: 'UK' },
-          })
-        ),
-      })
-    );
-    const bannerRequest = bannerRequestFactory.build({ edition: 'AU' });
-
-    const response = await st.post('/banner').send(bannerRequest);
-
-    expect(response.statusCode).toBe(200);
-    expect(response.body.assigned).toBeFalsy();
   });
 
   it('returns a 400 for an invalid payload', async () => {
-    const st = getSuperTest(
-      getRouter({
-        getBannerTests: getBannerTests(bannerTestFactory.build()),
-      })
-    );
     const bannerRequest = { foo: 'bar' };
 
-    const response = await st.post('/banner').send(bannerRequest);
+    const response = await request.post('/').send(bannerRequest);
 
     expect(response.statusCode).toBe(400);
   });
 });
-
-// ---- Utils ---- //
-
-const getSuperTest = (router: express.Router) => {
-  const app = express();
-  app.use('/banner', router);
-  return supertest(app);
-};
-
-const getBannerTests = (test: BannerTest) => () => Promise.resolve([test]);
--- apps/server/src/banner-assignment/router.ts
+++ apps/server/src/banner-assignment/router.ts
@@ -1,9 +1,9 @@
-import { BannerRequest, BannerTest } from '@sdc-libs/types';
+import { BannerTest } from '@sdc-libs/types';
 import { bannerRequestSchema } from '@sdc-libs/validation';
 import * as express from 'express';
-import { assigned, notAssigned } from '../assignment';
+import { Request, Response } from 'express';
 import { validate } from '../validation';
-import { selectTest } from './selection';
+import { getCreate } from './controller';
 
 interface RouterOptions {
   getBannerTests: () => Promise<BannerTest[]>;
@@ -16,28 +16,17 @@ export function getRouter({ getBannerTests }: RouterOptions) {
     '/',
     express.json(),
     validate(bannerRequestSchema),
-    async (req, res) => {
-      const body = req.body as BannerRequest;
-
-      const tests = await getBannerTests();
-      const test = selectTest(tests, body);
-
-      if (!test) {
-        return res.json(notAssigned());
-      }
-
-      res.json(
-        assigned({
-          meta: {
-            testName: test.name,
-          },
-          props: {
-            copy: test.copy,
-          },
-        })
-      );
-    }
+    makeJsonEndpoint(getCreate(getBannerTests))
   );
 
   return router;
 }
+
+// ---- Utils ---- //
+
+const makeJsonEndpoint =
+  <Req, Res>(controllerAction: (req: Req) => Promise<Res>) =>
+  async (req: Request, res: Response) => {
+    const json = await controllerAction(req.body as Req);
+    res.json(json);
+  };
